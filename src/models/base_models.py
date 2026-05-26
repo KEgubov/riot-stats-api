7a594b766ba6a7a1555054720505c83b300853f6
KEgubov <kirilegubov@gmail.com>
2026-05-26 16:39:51 +0200

    fix: Fixed field types for models, improved readability

--- src/models/base_models.py
+++ src/models/base_models.py
@@ -1,16 +1,30 @@
 import datetime
 from typing import Annotated
 
-from sqlalchemy import Text, ForeignKey, Index, JSON
+from sqlalchemy import Text, ForeignKey, Index, String, DateTime
+from sqlalchemy.dialects.postgresql.json import JSONB
 from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped, relationship
 
+str_10 = Annotated[str, 10]
+str_20 = Annotated[str, 20]
+str_30 = Annotated[str, 30]
+str_40 = Annotated[str, 40]
+str_80 = Annotated[str, 80]
+str_100 = Annotated[str, 100]
+
 intpk = Annotated[int, mapped_column(primary_key=True, autoincrement=True)]
-strpk = Annotated[str, mapped_column(primary_key=True)]
+strpk = Annotated[str_80, mapped_column(primary_key=True)]
 text = Annotated[str, mapped_column(Text)]
 
 
 class Base(DeclarativeBase):
-    pass
+    type_annotation_map = {
+        str_20: String(20),
+        str_30: String(30),
+        str_40: String(40),
+        str_80: String(80),
+        str_100: String(100),
+    }
 
     def __repr__(self):
         cols = []
@@ -25,7 +39,7 @@ class Player(Base):
     puuid: Mapped[strpk]
     game_name: Mapped[text]
     tag_line: Mapped[text]
-    updated_at: Mapped[datetime.datetime]
+    updated_at: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True))
 
     __table_args__ = (Index("idx_game_name_tag_line", "game_name", "tag_line"),)
 
@@ -38,9 +52,12 @@ class League(Base):
     __tablename__ = "leagues"
 
     id: Mapped[intpk]
-    puuid: Mapped[str] = mapped_column(ForeignKey("players.puuid"))
-    tier: Mapped[text]
-    rank: Mapped[text]
+    puuid: Mapped[str_80] = mapped_column(
+        ForeignKey("players.puuid", ondelete="CASCADE")
+    )
+    queue_type: Mapped[str_30]
+    tier: Mapped[str_20]
+    rank: Mapped[str_10]
     league_points: Mapped[int]
     wins: Mapped[int]
     losses: Mapped[int]
@@ -49,10 +66,10 @@ class League(Base):
 class Match(Base):
     __tablename__ = "matches"
 
-    match_id: Mapped[strpk]
-    game_version: Mapped[text]
-    game_creation: Mapped[datetime.datetime]
-    game_duration: Mapped[datetime.timedelta]
+    match_id: Mapped[str_40] = mapped_column(primary_key=True)
+    game_version: Mapped[str_20]
+    game_creation: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True))
+    game_duration: Mapped[int]
 
     participants: Mapped[list["MatchParticipant"]] = relationship(
         back_populates="match"
@@ -63,18 +80,20 @@ class MatchParticipant(Base):
     __tablename__ = "match_participants"
 
     id: Mapped[intpk]
-    match_id: Mapped[str] = mapped_column(
-        ForeignKey("matches.match_id", ondelete="CASCADE")
+    match_id: Mapped[str_40] = mapped_column(
+        ForeignKey("matches.match_id", ondelete="CASCADE"), index=True
     )
-    puuid: Mapped[str] = mapped_column(ForeignKey("players.puuid", ondelete="CASCADE"))
+    puuid: Mapped[str_80] = mapped_column(
+        ForeignKey("players.puuid", ondelete="CASCADE"), index=True
+    )
     champion_id: Mapped[int] = mapped_column(index=True)
     win: Mapped[bool]
     kills: Mapped[int]
     deaths: Mapped[int]
     assists: Mapped[int]
-    team_position: Mapped[str]
+    team_position: Mapped[str_20]
 
-    raw_data: Mapped[dict] = mapped_column(JSON)
+    raw_data: Mapped[dict] = mapped_column(JSONB)
 
     player: Mapped["Player"] = relationship(back_populates="match_stats")
     match: Mapped["Match"] = relationship(back_populates="participants")
