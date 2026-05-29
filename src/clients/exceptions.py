c3fb982616e42006724ca5610f27d2b585820355
KEgubov <kirilegubov@gmail.com>
2026-05-29 19:21:04 +0200

    feat: Added custom exception classes

--- src/clients/exceptions.py
+++ src/clients/exceptions.py
@@ -1,5 +1,30 @@
-class RiotKeyExpiredError(Exception):
+class RiotBaseException(Exception):
+    """Basic exception for all Riot API related errors"""
+
+    def __init__(self, message: str, error_code: str = None):
+        self.message = message
+        self.error_code = error_code
+        super().__init__(self.message)
+
+
+class RiotKeyExpiredError(RiotBaseException):
     """
     Raised when the Riot Development API key has expired or is invalid (403).
     """
+
+    pass
+
+
+class RiotRateLimitException(RiotBaseException):
+    """The limit of attempts when accessing the API has been reached (429)"""
+
+    pass
+
+
+class RiotServiceUnavailableException(RiotBaseException):
+    """
+    We throw it away if the external network is down or Riot has
+    a status of 5xx after all retrays
+    """
+
     pass
